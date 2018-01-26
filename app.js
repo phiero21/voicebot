08fb352ee118c6b0b522c551f89cd9ab37fd60f7
phiero21 <33662628+phiero21@users.noreply.github.com>
2018-01-26 16:05:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function isChrome() {
+﻿function isChrome() {
   var isChromium = window.chrome,
     winNav = window.navigator,
     vendorName = winNav.vendor,
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Now we’ve established that the browser is Chrome with proper speech API-s.
 
   // api.ai client
-  const apiClient = new ApiAi.ApiAiClient({accessToken: '329dcb8e2a8f4876acbf7fb616978686'});
+  const apiClient = new ApiAi.ApiAiClient({accessToken: '6eea38191541458e8a6a4dd22d266e25'});
 
   // Initial feedback message.
   addBotItem("Hi! I’m voicebot. Tap the microphone and start talking to me.");
